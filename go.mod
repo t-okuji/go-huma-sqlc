8d3afe43d2268631d457d715f70f3c67a60fa634
t-okuji <t.okuji34@gmail.com>
2025-01-09 15:06:25 +0900

    feat: first init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/t-okuji/learn-huma
+
+go 1.23.3
+
+require github.com/danielgtaylor/huma/v2 v2.27.0
+
+require (
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humago"
+
+	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
+)
+
+// GreetingOutput represents the greeting operation response.
+type GreetingOutput struct {
+	Body struct {
+		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
+	}
+}
+
+func main() {
+	// Create a new router & API
+	router := http.NewServeMux()
+	api := humago.New(router, huma.DefaultConfig("My API", "1.0.0"))
+
+	// Register GET /greeting/{name} handler.
+	huma.Get(api, "/greeting/{name}", func(ctx context.Context, input *struct {
+		Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+	}) (*GreetingOutput, error) {
+		resp := &GreetingOutput{}
+		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+		return resp, nil
+	})
+	// Start the server!
+	http.ListenAndServe(":8888", router)
+}
