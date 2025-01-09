d0ed9b3d160940fbf284390d6b08cbb38f8bf554
t-okuji <t.okuji34@gmail.com>
2025-01-09 16:09:57 +0900

    feat: Use cli

--- go.mod
+++ go.mod
@@ -6,5 +6,8 @@ require github.com/danielgtaylor/huma/v2 v2.27.0
 
 require (
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 )
--- main.go
+++ main.go
@@ -7,9 +7,15 @@ import (
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humago"
+	"github.com/danielgtaylor/huma/v2/humacli"
 
 	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
 )
+
+// Options for the CLI.
+type Options struct {
+	Port int `help:"Port to listen on" short:"p" default:"8888"`
+}
 
 // GreetingOutput represents the greeting operation response.
 type GreetingOutput struct {
@@ -19,25 +25,35 @@ type GreetingOutput struct {
 }
 
 func main() {
-	// Create a new router & API
-	router := http.NewServeMux()
-	api := humago.New(router, huma.DefaultConfig("My API", "1.0.0"))
+	// Create a CLI app which takes a port option.
+	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
+		// Create a new router & API
+		router := http.NewServeMux()
+		api := humago.New(router, huma.DefaultConfig("My API", "1.0.0"))
 
-	// Register GET /greeting/{name} handler.
-	huma.Register(api, huma.Operation{
-		OperationID: "get-greeting",
-		Method:      http.MethodGet,
-		Path:        "/greeting/{name}",
-		Summary:     "Get a greeting",
-		Description: "Get a greeting for a person by name.",
-		Tags:        []string{"Greetings"},
-	}, func(ctx context.Context, input *struct {
-		Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
-	}) (*GreetingOutput, error) {
-		resp := &GreetingOutput{}
-		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-		return resp, nil
+		// Register GET /greeting/{name} handler.
+		huma.Register(api, huma.Operation{
+			OperationID: "get-greeting",
+			Method:      http.MethodGet,
+			Path:        "/greeting/{name}",
+			Summary:     "Get a greeting",
+			Description: "Get a greeting for a person by name.",
+			Tags:        []string{"Greetings"},
+		}, func(ctx context.Context, input *struct {
+			Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+		}) (*GreetingOutput, error) {
+			resp := &GreetingOutput{}
+			resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+			return resp, nil
+		})
+		// Tell the CLI how to start your server.
+		hooks.OnStart(func() {
+			fmt.Printf("Starting server on port %d...\n", options.Port)
+			// Start the server!
+			http.ListenAndServe(fmt.Sprintf(":%d", options.Port), router)
+		})
 	})
-	// Start the server!
-	http.ListenAndServe(":8888", router)
+
+	// Run the CLI. When passed no commands, it starts the server.
+	cli.Run()
 }
