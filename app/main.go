0ef1a1959bc05acd6d6dea5dfcdd5afe609144c2
t-okuji <t.okuji34@gmail.com>
2025-01-16 15:55:15 +0900

    refactor: Delete unused code

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humago"
@@ -24,54 +25,9 @@ type Options struct {
 	Port int `help:"Port to listen on" short:"p" default:"8888"`
 }
 
-// GreetingOutput represents the greeting operation response.
-type GreetingOutput struct {
-	Body struct {
-		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
-	}
-}
-
-// ReviewInput represents the review operation request.
-type ReviewInput struct {
-	Body struct {
-		Author  string `json:"author" maxLength:"10" doc:"Author of the review"`
-		Rating  int    `json:"rating" minimum:"1" maximum:"5" doc:"Rating from 1 to 5"`
-		Message string `json:"message,omitempty" maxLength:"100" doc:"Review message"`
-	}
-}
-
-func sampleRoutes(api huma.API) {
-	// Register GET /greeting/{name} handler.
-	huma.Register(api, huma.Operation{
-		OperationID: "get-greeting",
-		Method:      http.MethodGet,
-		Path:        "/greeting/{name}",
-		Summary:     "Get a greeting",
-		Description: "Get a greeting for a person by name.",
-		Tags:        []string{"Greetings"},
-	}, func(ctx context.Context, input *struct {
-		Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
-	}) (*GreetingOutput, error) {
-		resp := &GreetingOutput{}
-		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-		return resp, nil
-	})
-
-	// Register POST /reviews
-	huma.Register(api, huma.Operation{
-		OperationID:   "post-review",
-		Method:        http.MethodPost,
-		Path:          "/reviews",
-		Summary:       "Post a review",
-		Tags:          []string{"Reviews"},
-		DefaultStatus: http.StatusCreated,
-	}, func(ctx context.Context, i *ReviewInput) (*struct{}, error) {
-		// TODO: save review in data store.
-		return nil, nil
-	})
-}
-
 func LogMiddleware(ctx huma.Context, next func(huma.Context)) {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	log.Info().Dict("api", zerolog.Dict().
 		Str("method", ctx.Operation().Method).
 		Str("path", ctx.Operation().Path),
@@ -97,8 +53,6 @@ func main() {
 		api_router := http.NewServeMux()
 		api := humago.New(api_router, huma.DefaultConfig("My API", "1.0.0"))
 		api.UseMiddleware(LogMiddleware)
-
-		sampleRoutes(api)
 
 		router.NewAuthorRouter(api, authorController)
 
